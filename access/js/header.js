3f9a003e142ee393adaee0047458f941633c7269
Huy nguyen <cushin2302@gmail.com>
2022-09-23 22:52:27 +0700

    huy

--- access/js/header.js
+++ access/js/header.js
@@ -23,7 +23,6 @@ function getTime() {
     } else if (minutes >= 10 && minutes <= 60) {
         minutes = `${minutes}`
     }
-    console.log(hours)
     if (hours <= 12) {
         hours < 10 ? timeNow += `0${hours}:${minutes} AM` : timeNow += `${hours}:${minutes} AM`
     } else if (hours > 12 && hours <= 24) {
@@ -120,42 +119,44 @@ $('.real-time').onclick = function () {
 dragElement(document.getElementById("realtime-content"));
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
     /* if present, the header is where you move the DIV from:*/
     document.getElementById("realtime-content").onmousedown = dragMouseDown;
-    
-  
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    console.log(pos1, pos2, pos3, pos4)
-  }
 
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-  
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        $('#realtime-content').style.cursor = 'grabbing';
+
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+        $('#realtime-content').style.cursor = 'grab';
+    }
 }
