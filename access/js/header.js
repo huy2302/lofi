1f4a583ad02ff0226bc1835ee828a637d413993c
Huy nguyen <cushin2302@gmail.com>
2023-03-15 22:36:28 +0700

    s

--- access/js/header.js
+++ access/js/header.js
@@ -115,8 +115,45 @@ $('.real-time').onclick = function () {
         $('.realtime-content').classList.add('realtime-active')
     }
 }
+dragTabFunction(document.getElementById('navbar-content'))
 
-dragElement(document.getElementById("realtime-content"));
+function dragTabFunction(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById("navbar-header").onmousedown = dragMouseDown;
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+    // start moving when mouse button is released:
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        $('#navbar-header').style.cursor = 'grabbing';
+    }
+    // stop moving when mouse button is released:
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+        $('#navbar-header').style.cursor = 'grab';
+    }
+}
+
+dragElement(document.getElementById("realtime-content"))
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
